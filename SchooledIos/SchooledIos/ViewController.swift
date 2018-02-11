fc6d68988fdfa21a8887908e7814a263bbf29fbd
Jerun Trajko <trajko2@illinois.edu>
2018-02-10 22:48:54 -0600

    Created IOS Project.

---
+++ SchooledIos/SchooledIos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SchooledIos
+//
+//  Created by Jerun Trajko on 2/10/18.
+//  Copyright © 2018 Jerun Trajko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
