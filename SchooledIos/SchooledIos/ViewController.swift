7b42e2afe2405e8e9e3f87b0eeb81507680c5bd1
Jerun Trajko <trajko2@illinois.edu>
2018-02-11 23:22:09 -0600

    Button on main view, sign up styled.

--- SchooledIos/SchooledIos/ViewController.swift
+++ SchooledIos/SchooledIos/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var SignInWithEmailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Update Button Style
+        SignInWithEmailButton.layer.borderWidth = 2
+        SignInWithEmailButton.layer.borderColor = UIColor.white.cgColor
+        SignInWithEmailButton.layer.shadowColor = UIColor.black.cgColor
+        SignInWithEmailButton.layer.shadowOffset = CGSize(width: 0.25, height: 0.25)
+        SignInWithEmailButton.layer.shadowOpacity = 1.0
+        SignInWithEmailButton.layer.shadowRadius = 0.0
     }
 
     override func didReceiveMemoryWarning() {
