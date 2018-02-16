78304f0a0b327643440bdc95a53ccf84b435e196
Jerun Trajko <trajko2@illinois.edu>
2018-02-16 00:13:37 -0600

    Updated more designs on login page, some shadow changes

--- SchooledIos/SchooledIos/Utilities/ElementStyling/ButtonStyling.swift
+++ SchooledIos/SchooledIos/Utilities/ElementStyling/ButtonStyling.swift
@@ -15,8 +15,8 @@ class ButtonStyling {
     func defaultStyling(button: UIButton) {
         button.layer.borderWidth = 2
         button.layer.borderColor = UIColor.white.cgColor
-        button.layer.shadowColor = UIColor.black.cgColor
-        button.layer.shadowOffset = CGSize(width: 0.25, height: 0.25)
+        button.layer.shadowColor = UIColor.gray.cgColor
+        button.layer.shadowOffset = CGSize(width: -0.8, height: 0.8)
         button.layer.shadowOpacity = 1.0
         button.layer.shadowRadius = 0.0
     }
