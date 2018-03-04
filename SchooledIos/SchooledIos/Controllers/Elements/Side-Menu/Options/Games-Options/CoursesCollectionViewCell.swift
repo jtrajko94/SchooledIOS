9307571303ff70b6baca97f80a861480e87f5369
Jerun Trajko <trajko2@illinois.edu>
2018-03-04 01:33:56 -0600

    collection view added to course selection

---
+++ SchooledIos/SchooledIos/Controllers/Elements/Side-Menu/Options/Games-Options/CoursesCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CoursesCollectionViewCell.swift
+//  SchooledIos
+//
+//  Created by Jerun Trajko on 3/4/18.
+//  Copyright © 2018 Jerun Trajko. All rights reserved.
+//
+
+import UIKit
+
+class CoursesCollectionViewCell: UICollectionViewCell {    
+    @IBOutlet weak var _cellButton: UIButton!
+}
--- SchooledIos/SchooledIos/Controllers/Elements/Side-Menu/Options/Games-Options/SubjectsViewController.swift
+++ SchooledIos/SchooledIos/Controllers/Elements/Side-Menu/Options/Games-Options/SubjectsViewController.swift
@@ -8,7 +8,24 @@
 
 import UIKit
 
-class SubjectsViewController: UIViewController {
+class SubjectsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate  {
+    
+    let items = ["0", "1", "2"]
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CoursesCollectionViewCell
+        cell._cellButton.setTitle(items[indexPath.item], for: .normal)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
