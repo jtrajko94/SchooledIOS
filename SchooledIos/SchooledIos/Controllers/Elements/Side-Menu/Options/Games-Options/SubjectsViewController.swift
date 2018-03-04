3e93db6b8b85edad7c3c0e2cd6f1ecd3aa5586e6
Jerun Trajko <trajko2@illinois.edu>
2018-03-04 12:16:41 -0600

    implemented subject and course selection. Still need to push course id to the game selection screen.

--- SchooledIos/SchooledIos/Controllers/Elements/Side-Menu/Options/Games-Options/SubjectsViewController.swift
+++ SchooledIos/SchooledIos/Controllers/Elements/Side-Menu/Options/Games-Options/SubjectsViewController.swift
@@ -11,35 +11,36 @@ import UIKit
 class SubjectsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate  {
     
     @IBOutlet weak var _coursesCollectionView: UICollectionView!
-    //These are the subjects. Initially loaded, get from SQL
-    let subjects = ["Math", "Science", "English"]
-    
-    //These are loaded after picking a subject, get from SQL
-    let courses = ["Back", "Algebra", "ICA", "Geometry", "Calculus"]
+    //IDs is list of subject internal ids, Mappings maps those ids with icons
+    //SQL will pull list of all ids, and create mapping url on collection view load
+    let subjectIds = [1, 2, 3]
+    let subjectMappings = [1: "prize", 2:"prize", 3:"prize"]
+    let courseIds = [0,1,2,3,4,5]
+    let courseMappings = [0:"ticket", 1: "present", 2:"present", 3:"present", 4: "present", 5: "present"]
     
     //use this if a subject has been picked
-    var subject = ""
+    var subjectSelected = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if(subject != ""){
-            return courses.count
+        if(subjectSelected != 0){
+            return courseIds.count
         }
-        return subjects.count
+        return subjectIds.count
     }
     
     //Change button based on the subject selected. The Subject titles should be the subject text for the courses, that you use to query in SQL
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CoursesCollectionViewCell
-        if(subject != ""){
-            cell._cellButton.setTitle(courses[indexPath.item], for: .normal)
-            cell._cellButton.setImage(UIImage(named: "present"), for: UIControlState.normal)
+        if(subjectSelected != 0){
+            cell._cellButton.tag = courseIds[indexPath.item]
+            cell._cellButton.setImage(UIImage(named: courseMappings[cell._cellButton.tag]!), for: UIControlState.normal)
         } else {
-            cell._cellButton.setTitle(subjects[indexPath.item], for: .normal)
-            cell._cellButton.setImage(UIImage(named: "prize"), for: UIControlState.normal)
+            cell._cellButton.tag = subjectIds[indexPath.item]
+            cell._cellButton.setImage(UIImage(named: subjectMappings[cell._cellButton.tag]!), for: UIControlState.normal)
         }
         
         cell._cellButton.addTarget(self, action: #selector(cellButtonClicked), for: UIControlEvents.touchUpInside)
@@ -50,8 +51,12 @@ class SubjectsViewController: UIViewController, UICollectionViewDataSource, UICo
         guard let button = sender as? UIButton else {
             return
         }
-
-        subject = button.title(for: .normal)!
-        _coursesCollectionView.reloadData()
+        
+        if(subjectSelected == 0 || button.tag == 0){
+            subjectSelected = button.tag
+            _coursesCollectionView.reloadData()
+        }else{
+            performSegue(withIdentifier: "gameSelectionSegue", sender: button)
+        }
     }
 }
