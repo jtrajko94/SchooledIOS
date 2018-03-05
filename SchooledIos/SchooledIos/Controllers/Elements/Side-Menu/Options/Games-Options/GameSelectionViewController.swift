a6646dc8392bddc5d2094e6aa85db1cf6e77c5ca
Jerun Trajko <trajko2@illinois.edu>
2018-03-04 23:13:49 -0600

    Game view table view completed.

--- SchooledIos/SchooledIos/Controllers/Elements/Side-Menu/Options/Games-Options/GameSelectionViewController.swift
+++ SchooledIos/SchooledIos/Controllers/Elements/Side-Menu/Options/Games-Options/GameSelectionViewController.swift
@@ -8,15 +8,37 @@
 
 import UIKit
 
-class GameSelectionViewController: UIViewController {
-
+class GameSelectionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     //This will be updated on load to the course selected
     var courseId: Int = 0
     var difficulty: Int = 0
+    
+    let list = ["Game 1", "Game 2", "Game 3", "Game 4", "Game 5"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print(courseId)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "gameCell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        return 200;
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(list[indexPath.row])
     }
 }
